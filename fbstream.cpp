abc346acdcab5ff9b9d976d5bf94aaf14fa56851
Masahiko AMANO <hiko-19@yandex.com>
2022-09-29 16:07:25 +0300

    Add EOF checking.

--- fbstream.cpp
+++ fbstream.cpp
@@ -8,6 +8,9 @@ unsigned long long int ifbstream::read(unsigned char len) {
 	if (len > 64) {
 		throw std::length_error("Too big length");
 	}
+	if (_fstream.eof()) {
+		throw std::out_of_range("Length out of range");
+	}
 	unsigned long long int out = 0;
 	if (_bitpos < len) {
 		out |= _buffer[0] & ((1 << _bitpos) - 1);
@@ -15,15 +18,26 @@ unsigned long long int ifbstream::read(unsigned char len) {
 		_fstream.read(_buffer, 1);
 		_bitpos = 8;
 	}
+	if (_fstream.eof() && len) {
+		throw std::out_of_range("Length out of range");
+	}
 	while (len > 8) {
+		if (_fstream.eof()) {
+			throw std::out_of_range("Length out of range");
+		}
 		out = out << 8 | _buffer[0];
 		len -= 8;
 		_fstream.read(_buffer, 1);
 	}
+	if (len) {
+		if (_fstream.eof()) {
+			throw std::out_of_range("Length out of range");
+		}
 	out = out << len | _buffer[0] >> _bitpos - len & ((1 << len) - 1);
 	if ((_bitpos -= len) == 0) {
 		_fstream.read(_buffer, 1);
 		_bitpos = 8;
+	}
 	}
 	return out;
 }
