6d4a69970f83eed6b06215c4fd38112052b7cbb4
Masahiko AMANO <hiko-19@yandex.com>
2022-10-01 14:35:07 +0300

    Bug fix.

--- fbstream.cpp
+++ fbstream.cpp
@@ -99,12 +99,15 @@ void ofbstream::write(unsigned long long int data, unsigned char blocksize) {
 		_buffer[0] = _buffer[0] << blocksize | data & (1 << blocksize) - 1;
 		_bitpos -= blocksize;
 		if (!_bitpos) {
+			_fstream.write(_buffer, 1);
 			_bitpos = 8;
 		}
 	}
 }
 void ofbstream::close() {
-	_buffer[0] <<= _bitpos;
-	_fstream.write(_buffer, 1);
+	if (_bitpos != 8) {
+		_buffer[0] <<= _bitpos;
+		_fstream.write(_buffer, 1);
+	}
 	_fstream.close();
 }
